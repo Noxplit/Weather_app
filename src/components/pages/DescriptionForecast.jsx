53c2110a1e44522920bb088a2becb63410d730e3
Denis <kip22ops@gmail.com>
2023-07-16 21:42:02 +0300

    four

--- src/components/pages/DescriptionForecast.jsx
+++ src/components/pages/DescriptionForecast.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux"
 import DescriptionWeather from "../Description/DescriptionWeather/DescriptionWeather";
 import DayForecast from "../Description/ForTenHour/DayForecast";
+import styles from './descriptionForecast.module.scss'
 
 const DescriptionForecast = () => {
   const {oneDayForecast} = useSelector(state=>state)
@@ -11,7 +12,7 @@ const DescriptionForecast = () => {
   return (
     <div>
 <DescriptionWeather weather={weather}  oneDayForecast={oneDayForecast} />
-<div style={{display:'flex', flexWrap:'wrap', gap:'50px', justifyContent:'space-around', alignItems:'center'}}>{dayForecast?.map(forecast => <DayForecast forecast={forecast}/>)}</div>
+<div className={styles.wrapper}>{dayForecast?.map(forecast => <DayForecast forecast={forecast}/>)}</div>
     </div>
   )
 }
